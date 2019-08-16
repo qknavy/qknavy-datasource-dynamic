0d3b75c11fd4ef9dde2f75a352ab0f1d1667cfe1
qknavy_ser <qknavy_ser@aliyun.com>
2019-08-16 16:36:54 +0800

    添加一个单元测试

--- src/main/java/com/qknavy/users/controller/UserController.java
+++ src/main/java/com/qknavy/users/controller/UserController.java
@@ -8,10 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(value = "/users")
@@ -24,11 +21,7 @@ public class UserController {
     @RequestMapping(value = "/{dbid}/add", method = RequestMethod.POST)
     @Dynamic
     @ResponseBody
-    public String add(@DynamicIdentifier @PathVariable(value = "dbid") String dbid, String name, int age, double money){
-        User user = new User();
-        user.setAge(age);
-        user.setMoney(money);
-        user.setName(name);
+    public String add(@DynamicIdentifier @PathVariable(value = "dbid") String dbid, @RequestBody User user){
         userService.save(user);
         return "success";
     }
---
+++ src/test/java/com/qknavy/users/controller/UserControllerTest.java
@@ -0,0 +1,54 @@
+package com.qknavy.users.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.qknavy.BaseTest;
+import com.qknavy.users.entity.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.*;
+
+@AutoConfigureMockMvc
+public class UserControllerTest extends BaseTest{
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void before() {
+        //获取mockmvc对象实例
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+
+    @Test
+    public void add() {
+        User user = new User();
+        user.setAge(21);
+        user.setMoney(3800);
+        user.setName("小刘");
+        String postBody = JSON.toJSON(user).toString();
+        try {
+            mockMvc.perform(MockMvcRequestBuilders.post("/users/10001/add")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content(postBody))
+                    .andExpect(MockMvcResultMatchers.status().is(200));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
